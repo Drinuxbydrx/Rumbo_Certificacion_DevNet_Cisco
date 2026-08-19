6f4d0bb3d0da7c9ff195b3021e701f9694a84b25
drinuxblueteam <106790614+drinuxblueteam@users.noreply.github.com>
2026-08-19 04:22:14 -0600

    Agregando Ejercicio 1 rumbo a la certificacion Devnet

---
+++ Capitulo-1/Ejercicicios/Class/ExtractorDispositivos.py
@@ -0,0 +1,13 @@
+import csv
+
+class ExtractorDispositivos:
+    def __init__(self,ruta_archivo_csv):
+        self.__ruta_archivo_csv = ruta_archivo_csv
+        self.__nombre_dispositivo="router"
+    def extractordedispositivos(self):
+        archivo_csv = open("dispositivos.csv")
+        lector = csv.reader(archivo_csv)
+        for fila in lector:
+            if self.__nombre_dispositivo in fila:
+                print(fila)
+        archivo_csv.close()
---
+++ Capitulo-1/Ejercicicios/Ejercicio1-manipulacion-CSV.py
@@ -0,0 +1,24 @@
+""" 
+Se debe crear un archivo nombrado dispositivos.csv 
+y que contenga la siguiente informacion 
+
+hostname,ip,device_type
+R1,192.168.1.1,router
+SW1,192.168.1.2,switch
+R2,192.168.1.3,router
+
+El programa debe 
+1.-Abrir el archivo utilizando open().
+2.-Utilizar el módulo csv.
+3.-Leer las líneas.
+4.-Mostrar únicamente los dispositivos cuyo device_type sea "router"
+5.-Cerrar el archivo utilizando close().
+"""
+from Class.ExtractorDispositivos import ExtractorDispositivos
+
+if __name__ == "__main__":
+
+    ruta="."
+
+    extractor = ExtractorDispositivos(ruta)
+    extractor.extractordedispositivos()
